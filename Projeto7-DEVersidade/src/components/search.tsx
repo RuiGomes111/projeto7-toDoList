5c51528e6b6d590d91e22dad0266bf18cfd8a1cd
Rui Gomes <ruisoft01@gmail.com>
2025-08-25 23:11:43 +0100

    bug fix1

--- Projeto7-DEVersidade/src/components/search.tsx
+++ Projeto7-DEVersidade/src/components/search.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const search = ({search, setSearch}) => {
   return (
-    <div className=''>
+    <div >
       <h2>Pesquisar Tarefa:</h2>
       <input 
         type="text" 
         value={search}
         onChange={(e)=>setSearch(e.target.value)}
         placeholder='Pesquisa a sua tarefa'
-        className='flex px-2 w-full m-2'
+        className='flex  w-full mb-2'
         />
     </div>
   )
